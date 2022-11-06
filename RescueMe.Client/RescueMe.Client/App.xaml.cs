3244e50f3f3cf5016c3375fda7cbca32db1f4910
SintoGenroku <borozda.a.s@gmail.com>
2022-11-06 17:36:47 +0300

    add empty projects and gitignore

---
+++ RescueMe.Client/RescueMe.Client/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace RescueMe.Client;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ RescueMe.Client/RescueMe.Client/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace RescueMe.Client;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
